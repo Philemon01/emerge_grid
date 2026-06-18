1df1e0088f0f5fed63240bd78cb248523dbd2e1b
Philemon Progress Michael <108028744+Philemon01@users.noreply.github.com>
2026-06-18 20:17:10 +0100

    Uncomment ProfileCheck component in header

--- my-app/app/components/header.tsx
+++ my-app/app/components/header.tsx
@@ -31,7 +31,7 @@ function ProfileCheck() {
         className="p-2 rounded-full hover:bg-gray-100 transition-colors focus:outline-none"
       >
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-gray-700">
-          <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+          <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.9[...]
         </svg>
       </button> 
 
@@ -87,7 +87,7 @@ export default function Header() {
           </div>
 
           {/* Profile Dropdown */}
-          {/* <ProfileCheck /> */}
+          <ProfileCheck />
         </div>
       </div>
     </motion.header>
